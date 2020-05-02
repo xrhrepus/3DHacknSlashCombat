4fe01198b15a240b7985c7b4635db8357690f2f3
xrhrepus <xrhrepus@gmail.com>
2020-05-01 20:50:44 -0700

    add new player

---
+++ 3Dcombat/modeltest/Assets/Scripts/UnityChan/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public bool inFight { get; set; }
+
+    void Update()
+    {
+        
+    }
+}
