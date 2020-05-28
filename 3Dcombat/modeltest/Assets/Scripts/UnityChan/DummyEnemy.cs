bcb4e85e21b7bb9c54a86c80958b2bf9d54b862e
xrhrepus <xrhrepus@gmail.com>
2020-05-27 18:29:53 -0700

    add, add dummy enemy

---
+++ 3Dcombat/modeltest/Assets/Scripts/UnityChan/DummyEnemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class DummyEnemy : MonoBehaviour
+{
+
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+
+    [SerializeField] private Transform _from;
+    [SerializeField] private Transform _to;
+    private Vector3 _destination;
+    public bool _move = false;
+    public bool toTo = true;
+    private void Awake()
+    {
+        //_navMeshAgent = GetComponent<NavMeshAgent>();
+        //_destination = _to.position;
+        //_move = true;
+        //_navMeshAgent.isStopped = _move;
+    }
+ 
+    void Update()
+    {
+        if (_move)
+        {
+            if (Vector3.Distance(transform.position, _destination) < 0.5f)
+            {
+                _destination = toTo ? _to.position : _from.position;
+                _navMeshAgent.SetDestination(_destination);
+                toTo = !toTo;
+            }
+        }
+
+    }
+}
