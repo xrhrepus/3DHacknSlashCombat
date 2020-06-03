793873da41334f1438595d42534739d022a5bbbb
xrhrepus <xrhrepus@gmail.com>
2020-06-03 16:37:44 -0700

    add,add game flow control scripts

---
+++ 3Dcombat/modeltest/Assets/Scripts/GameFlow/GameStarter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameStarter : MonoBehaviour
+{
+    public List<GameObject> _enemies;
+    public Canvas _canvas;
+
+    [SerializeField] private GameStarter_HitOnSkull _gameStarter_HitOnSkull;
+ 
+    private void Awake()
+    {
+        foreach (var e in _enemies)
+        {
+            e.SetActive(false);
+        }
+    }
+
+    public void GameStart()
+    {
+        foreach (var e in _enemies)
+        {
+            e.SetActive(true);
+        }
+        //_canvas.enabled = false;
+    }
+
+    public void Restart()
+    {
+        _gameStarter_HitOnSkull.enabled = true;
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void BackToMenu()
+    {
+ 
+        SceneManager.LoadSceneAsync(0);
+    }
+}
---
+++ 3Dcombat/modeltest/Assets/Scripts/GameFlow/GameStarter_HitOnSkull.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStarter_HitOnSkull : MonoBehaviour
+{
+    [SerializeField] private GameStarter _gameStarter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 12)
+        {
+            _gameStarter.GameStart();
+            this.enabled = false;
+        }
+
+    }
+}
