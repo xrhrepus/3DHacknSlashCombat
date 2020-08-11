edb6bcc2d08c0cdb666beb10af05e007e4e20ec9
xrhrepus <xrhrepus@gmail.com>
2020-08-11 12:53:38 -0700

    Add: add Knight script and its components

--- 3Dcombat/NewStyleAttemp/Assets/Script/Knight/Knight.cs
+++ 3Dcombat/NewStyleAttemp/Assets/Script/Knight/Knight.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class Knight : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private KnightCtrl _knightCtrl;
+    public KnightCtrl KnightCtrl { get=> _knightCtrl;}
+    [SerializeField]
+    private Knight_Transform _knightTransform;
+    public Knight_Transform KnightTransform { get => _knightTransform; }
+    [SerializeField]
+    private Knight_Animation _knightAnimation;
+    public Knight_Animation KnightAnimation { get => _knightAnimation; }
+    [SerializeField]
+    private Knight_Ability _knightAbility;
+    public Knight_Ability KnightAbility { get => _knightAbility; }
+    
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+     void Update()
     {
         
     }
