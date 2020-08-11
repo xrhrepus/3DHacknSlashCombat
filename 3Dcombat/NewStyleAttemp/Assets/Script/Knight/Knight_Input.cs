aac72ed16b03ed56be8b9372e8c486724e61066d
xrhrepus <xrhrepus@gmail.com>
2020-08-11 16:56:58 -0700

    Add: add prop to get characterCtrl

---
+++ 3Dcombat/NewStyleAttemp/Assets/Script/Knight/Knight_Input.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight_Input : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerInput _playerInput;
+    public InputCtrl.PlayerActions KnightInputCtrl { get => _playerInput.InputCtrl.Player; } 
+ 
+}
