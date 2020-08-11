e9f27f69b06c78a8ad845c6633b2ab3732f9b2a2
xrhrepus <xrhrepus@gmail.com>
2020-08-11 16:55:52 -0700

    Rep: rename KnightCtrl with PlayerInput(contains both character ctrl and later ui ctrl)

---
+++ 3Dcombat/NewStyleAttemp/Assets/Script/Knight/CharacterControl/PlayerInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private InputCtrl _inputCtrl;
+    public InputCtrl InputCtrl { get => _inputCtrl; }
+    private void OnEnable()
+    {
+        _inputCtrl.Enable();
+    }
+    private void OnDisable()
+    {
+        _inputCtrl.Disable();
+    }
+
+    private void Awake()
+    {
+        _inputCtrl = new InputCtrl();
+    }
+
+}
