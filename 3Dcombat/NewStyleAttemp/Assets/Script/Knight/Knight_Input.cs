d2ed5cf053c56fb5b0b9724c75b66a84f59a77fa
xrhrepus <xrhrepus@gmail.com>
2020-08-13 16:59:46 -0700

    Add: add move and attack status

--- 3Dcombat/NewStyleAttemp/Assets/Script/Knight/Knight_Input.cs
+++ 3Dcombat/NewStyleAttemp/Assets/Script/Knight/Knight_Input.cs
@@ -4,8 +4,44 @@ using UnityEngine;
 
 public class Knight_Input : MonoBehaviour
 {
+ 
     [SerializeField]
     private PlayerInput _playerInput;
-    public InputCtrl.PlayerActions KnightInputCtrl { get => _playerInput.InputCtrl.Player; } 
+    public InputCtrl.PlayerActions KnightInputCtrl { get => _playerInput.InputCtrl.Player; }
+    //
+    [SerializeField]
+    private Vector2 _inputDirection;
+    public Vector2 InputDirection { get => _inputDirection; }
+
+    [SerializeField]
+    private bool _inputAttack = false;
+    public bool InputAttack { get => _inputAttack; }
+
+    private void Awake()
+    {
+    }
+    private void Start()
+    {
+        KnightInputCtrl.Move.performed += _move => { ReadInputDirection(_move.ReadValue<Vector2>()); Perform_Move(); };
+        KnightInputCtrl.Attack.performed += _attackPerform => { _inputAttack = true; Perform_Attack(); };
+        KnightInputCtrl.Attack.canceled += _attackRelease => { _inputAttack = false; Release_Attack(); };
+
+    }
+    void ReadInputDirection(Vector2 dir)
+    {
+        _inputDirection = dir;
+    }
+    void Perform_Move()
+    {
+
+    }
+    void Perform_Attack()
+    {
+
+    }
+    void Release_Attack()
+    {
+
+    }
  
 }
