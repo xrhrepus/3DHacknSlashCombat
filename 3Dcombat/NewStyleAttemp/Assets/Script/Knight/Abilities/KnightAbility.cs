e896b1cd2e92466a2d8458b4255148dccf2a75fd
xrhrepus <xrhrepus@gmail.com>
2020-08-14 19:16:48 -0700

    Add: add abstract class Ability

---
+++ 3Dcombat/NewStyleAttemp/Assets/Script/Knight/Abilities/KnightAbility.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class KnightAbility : StateMachineBehaviour
+{
+    [SerializeField]
+    private Knight _knight;
+    public Knight Knight { get; set; }
+
+
+}
