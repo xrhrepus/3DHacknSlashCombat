6e5af44a6a6d562b854591e981e6630fb0cf9973
xrhrepus <xrhrepus@gmail.com>
2020-08-14 19:18:25 -0700

    Add: add ability list to init reference in each abilities in animator

---
+++ 3Dcombat/NewStyleAttemp/Assets/Script/Knight/Knight_AbilityList.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight_AbilityList : MonoBehaviour
+{
+    [SerializeField]
+    private Knight _knight;
+    public Knight Knight { get => _knight; }
+
+    private void Awake()
+    {
+        var abilityBehaviour = Knight.KnightAnimation.Animator.GetBehaviours<KnightAbility>();
+        foreach (var ab in abilityBehaviour)
+        {
+            ab.Knight = Knight;
+        }
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
