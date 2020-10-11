5c097ef6b02c459e63cdac87cb7cde04bdc86837
xrhrepus <xrhrepus@gmail.com>
2020-10-11 12:52:06 -0700

    Add, add ability control script

---
+++ 3Dcombat/NewStyleAttemp/Assets/Script/Knight/AbilityStateControl/Knight_AbilityState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+[Serializable]
+public class Knight_AbilityState
+{
+    [SerializeField]
+    private bool isIdling = false;
+    public bool IsIdling { get => isIdling; set => isIdling = value; }
+
+    [SerializeField]
+    private bool isMoving = false;
+    public bool IsMoving { get => isMoving; set => isMoving = value; }
+
+    [SerializeField]
+    private bool isOnGround = false;
+    public bool IsOnGround { get => isOnGround; set => isOnGround = value; }
+ 
+    [SerializeField]
+    private bool isAttacking = false;
+    public bool IsAttacking { get => isAttacking; set => isAttacking = value; }
+
+
+    public bool CanAttack { get; set; }
+    public bool CanMove { get; set; }
+    public bool CanJump { get; set; }
+}
---
+++ 3Dcombat/NewStyleAttemp/Assets/Script/Knight/AbilityStateControl/Knight_AbilityStateControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight_AbilityStateControl : MonoBehaviour
+{
+    [SerializeField]
+    private Knight_AbilityState _knightAbilityState;
+
+    private void Awake()
+    {
+        
+    }
+
+    public void MoveForwardPress()
+    { }
+    public void MoveBackwardPress()
+    { }
+    public void MoveLeftPress()
+    { }
+    public void MoveRightPress()
+    { }
+    public void AttackPress()
+    { }
+
+    public void AttackHold()
+    { }
+
+    public void AttackRelease()
+    { }
+
+}
